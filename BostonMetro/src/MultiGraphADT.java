c1b46d010ce8a8b39fe542086d386d6878741b44
AyodejiAkinyemi <ayodejiakinyemi1995@gmail.com>
2016-09-26 17:47:55 +0100

    Initial set up of project

---
+++ BostonMetro/src/MultiGraphADT.java
@@ -0,0 +1,20 @@
+
+public interface MultiGraphADT {
+	 
+	/**
+	 * This method adds a node object to a list by appending it
+	 * to the end of the list
+	 * 
+	 * @param node A node object to be added to the list
+	 *  
+	 */
+	public void addNode();
+	
+	public void removeNode();
+	
+	
+
+
+
+
+}
