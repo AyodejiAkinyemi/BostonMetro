fb6b39b298ba7bd4cdf01184c605d080df5dc1db
AyodejiAkinyemi <ayodejiakinyemi1995@gmail.com>
2016-09-26 18:03:56 +0100

    Modified file to include description for methods

--- BostonMetro/src/MultiGraphADT.java
+++ BostonMetro/src/MultiGraphADT.java
@@ -5,10 +5,17 @@ public interface MultiGraphADT {
 	 * This method adds a node object to a list by appending it
 	 * to the end of the list
 	 * 
-	 * @param node A node object to be added to the list
+	 * @param node a node object to be added to a list
 	 *  
 	 */
 	public void addNode();
+	
+	/**
+	 * This method removes a node object to a list by appending it
+	 * to the end of the list
+	 * 
+	 * @param node a node object to be added to a list 
+	 */
 	
 	public void removeNode();
 	
