7df24a7918b2cafd2cd04eb833a360600bf3b1c1
Stu Gilmour <stuart.gilmour.2014@uni.strath.ac.uk>
2016-09-26 21:32:02 +0100

    changed the comment in removeNode() to test push

--- BostonMetro/src/MultiGraphADT.java
+++ BostonMetro/src/MultiGraphADT.java
@@ -11,7 +11,7 @@ public interface MultiGraphADT {
 	public void addNode();
 	
 	/**
-	 * This method removes a node object to a list by appending it
+	 * This method removes a node object from a list by appending it
 	 * to the end of the list
 	 * 
 	 * @param node a node object to be added to a list 
